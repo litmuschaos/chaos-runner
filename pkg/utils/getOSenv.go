4c5c166601e171eeeba5a0a7bcdfdbb1d207e5bb
Rahul M Chheda <53308066+rahulchheda@users.noreply.github.com>
2020-03-27 12:00:15 +0530

    (feat) Added support for AdminMode (#45)
    * (feat) Added support for AdminMode
        - Replaced EngineDetails.AppNamespace to EngineDetails.EngineNamespace
        - Added logic to check for resources in chaosEngine namespace
        - Only job will be created in AppNamespace (experiment.Namespace)
        - Synced the experiment.Namespace = engineDetails.EngineNamespace, when AdminMode is true
        - Added OS ENV as ENGINE_NAMESPACE passed from operator, into engineDetails struct
    * Sycned experiment.Namespace and engineDetails.EngineNamespace
    
    Signed-off-by: Rahul M Chheda <rahul.chheda@mayadata.io>

--- pkg/utils/getOSenv.go
+++ pkg/utils/getOSenv.go
@@ -10,10 +10,16 @@ func GetOsEnv(engineDetails *EngineDetails) {
 	experimentList := os.Getenv("EXPERIMENT_LIST")
 	engineDetails.Name = os.Getenv("CHAOSENGINE")
 	engineDetails.AppLabel = os.Getenv("APP_LABEL")
-	engineDetails.AppNamespace = os.Getenv("APP_NAMESPACE")
+	engineDetails.EngineNamespace = os.Getenv("CHAOS_NAMESPACE")
 	engineDetails.AppKind = os.Getenv("APP_KIND")
 	engineDetails.SvcAccount = os.Getenv("CHAOS_SVC_ACC")
 	engineDetails.ClientUUID = os.Getenv("CLIENT_UUID")
 	engineDetails.Experiments = strings.Split(experimentList, ",")
 	engineDetails.AuxiliaryAppInfo = os.Getenv("AUXILIARY_APPINFO")
+
+	//TODO: Use engineDetails.AdminMode, to change behaviour of chaos-runner
+	// engineDetails.AdminMode = false
+	// if os.Getenv("ADMIN_MODE") == "true" {
+	// 	engineDetails.AdminMode = true
+	// }
 }
