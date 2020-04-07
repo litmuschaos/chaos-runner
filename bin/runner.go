5f1889234bdfc130bed9e0524a2a969aea45f2c8
Rahul M Chheda <rahul.chheda@mayadata.io>
2020-04-07 20:30:40 +0530

    (fix) ExperimentStatuses Fixed
    Signed-off-by: Rahul M Chheda <rahul.chheda@mayadata.io>

--- bin/runner.go
+++ bin/runner.go
@@ -19,35 +19,29 @@ func main() {
 	// Fetching all the ENV's needed
 	utils.GetOsEnv(&engineDetails)
 	klog.V(0).Infoln("Experiments List: ", engineDetails.Experiments, " ", "Engine Name: ", engineDetails.Name, " ", "appLabels : ", engineDetails.AppLabel, " ", "appKind: ", engineDetails.AppKind, " ", "Service Account Name: ", engineDetails.SvcAccount, "Engine Namespace: ", engineDetails.EngineNamespace)
-
+	experimentList := utils.CreateExperimentList(&engineDetails)
+	utils.InitialPatchEngine(engineDetails, clients, *experimentList)
 	recorder, err := utils.NewEventRecorder(clients, engineDetails)
 	if err != nil {
 		klog.Errorf("Unable to initiate EventRecorder for Chaos-Runner, would not be able to add events")
 	}
 	// Steps for each Experiment
-	for i := range engineDetails.Experiments {
+	for _, experiment := range *experimentList {
 
 		// Sending event to GA instance
 		if engineDetails.ClientUUID != "" {
-			analytics.TriggerAnalytics(engineDetails.Experiments[i], engineDetails.ClientUUID)
+			analytics.TriggerAnalytics(experiment.Name, engineDetails.ClientUUID)
 		}
-
-		experiment := utils.NewExperimentDetails(&engineDetails, i)
 
 		if err := experiment.SetValueFromChaosResources(&engineDetails, clients); err != nil {
 			klog.V(0).Infof("Unable to set values from Chaos Resources due to error: %v", err)
-			recorder.ExperimentSkipped(engineDetails.Experiments[i], utils.ExperimentNotFoundErrorReason)
+			recorder.ExperimentSkipped(experiment.Name, utils.ExperimentNotFoundErrorReason)
 		}
 
 		if err := experiment.SetENV(engineDetails, clients); err != nil {
 			klog.V(0).Infof("Unable to patch ENV due to error: %v", err)
-			recorder.ExperimentSkipped(engineDetails.Experiments[i], utils.ExperimentEnvParseErrorReason)
+			recorder.ExperimentSkipped(experiment.Name, utils.ExperimentEnvParseErrorReason)
 			break
-		}
-		experimentStatus := utils.ExperimentStatus{}
-		experimentStatus.InitialExperimentStatus(experiment)
-		if err := experimentStatus.InitialPatchEngine(engineDetails, clients); err != nil {
-			klog.V(0).Infof("Unable to set Initial Status in ChaosEngine, due to error: %v", err)
 		}
 
 		klog.V(0).Infof("Preparing to run Chaos Experiment: %v", experiment.Name)
@@ -56,34 +50,34 @@ func main() {
 			klog.V(0).Infof("Unable to patch Chaos Resources required for Chaos Experiment: %v, due to error: %v", experiment.Name, err)
 		}
 
-		recorder.ExperimentDepedencyCheck(engineDetails.Experiments[i])
+		recorder.ExperimentDepedencyCheck(experiment.Name)
 
 		// Creation of PodTemplateSpec, and Final Job
-		if err := utils.BuildingAndLaunchJob(experiment, clients); err != nil {
+		if err := utils.BuildingAndLaunchJob(&experiment, clients); err != nil {
 			klog.V(0).Infof("Unable to construct chaos experiment job due to: %v", err)
-			recorder.ExperimentSkipped(engineDetails.Experiments[i], utils.ExperimentJobCreationErrorReason)
+			recorder.ExperimentSkipped(experiment.Name, utils.ExperimentJobCreationErrorReason)
 			break
 		}
-		recorder.ExperimentJobCreate(engineDetails.Experiments[i], experiment.JobName)
+		recorder.ExperimentJobCreate(experiment.Name, experiment.JobName)
 
 		klog.V(0).Infof("Started Chaos Experiment Name: %v, with Job Name: %v", experiment.Name, experiment.JobName)
 		// Watching the Job till Completion
-		if err := engineDetails.WatchJobForCompletion(experiment, clients); err != nil {
+		if err := engineDetails.WatchJobForCompletion(&experiment, clients); err != nil {
 			klog.V(0).Infof("Unable to Watch the Job, error: %v", err)
-			recorder.ExperimentSkipped(engineDetails.Experiments[i], utils.ExperimentJobWatchErrorReason)
+			recorder.ExperimentSkipped(experiment.Name, utils.ExperimentJobWatchErrorReason)
 			break
 		}
 
 		// Will Update the chaosEngine Status
-		if err := engineDetails.UpdateEngineWithResult(experiment, clients); err != nil {
+		if err := engineDetails.UpdateEngineWithResult(&experiment, clients); err != nil {
 			klog.V(0).Infof("Unable to Update ChaosEngine Status due to: %v", err)
 		}
 
 		// Delete / retain the Job, using the jobCleanUpPolicy
-		jobCleanUpPolicy, err := engineDetails.DeleteJobAccordingToJobCleanUpPolicy(experiment, clients)
+		jobCleanUpPolicy, err := engineDetails.DeleteJobAccordingToJobCleanUpPolicy(&experiment, clients)
 		if err != nil {
 			klog.V(0).Infof("Unable to Delete ChaosExperiment Job due to: %v", err)
 		}
-		recorder.ExperimentJobCleanUp(experiment, jobCleanUpPolicy)
+		recorder.ExperimentJobCleanUp(&experiment, jobCleanUpPolicy)
 	}
 }
