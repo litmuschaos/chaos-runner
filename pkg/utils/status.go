862419ae02336b68a5d3426d43da4e7506a54101
Karthik Satchitanand <karthik.s@mayadata.io>
2020-07-31 22:12:21 +0530

    (enhancement)engine_status_patch:  enhance engine status patch logic based on updated schema (#76)
    * (enhancement)engine_status_patch: enhance engine statuspatch logic based on updated schema
    
    Signed-off-by: ksatchit <karthik.s@mayadata.io>

--- pkg/utils/status.go
+++ pkg/utils/status.go
@@ -6,35 +6,42 @@ import (
 )
 
 // InitialExperimentStatus fills up ExperimentStatus Structure with InitialValues
-func (expStatus *ExperimentStatus) InitialExperimentStatus(jobName string) {
-	expStatus.Name = jobName
+func (expStatus *ExperimentStatus) InitialExperimentStatus(expName, engineName string) {
+	expStatus.Name = expName
+	expStatus.Runner = engineName + "-runner"
+	expStatus.ExpPod = "Yet to be launched"
 	expStatus.Status = "Waiting for Job Creation"
-	expStatus.Verdict = "Waiting"
+	expStatus.Verdict = "N/A"
 	expStatus.LastUpdateTime = metav1.Now()
 }
 
 // AwaitedExperimentStatus fills up ExperimentStatus Structure with Running Status
-func (expStatus *ExperimentStatus) AwaitedExperimentStatus(experimentDetails *ExperimentDetails) {
-	expStatus.Name = experimentDetails.JobName
+func (expStatus *ExperimentStatus) AwaitedExperimentStatus(expName, engineName, experimentPodName string) {
+	expStatus.Name = expName
+	expStatus.Runner = engineName + "-runner"
+	expStatus.ExpPod = experimentPodName
 	expStatus.Status = "Running"
 	expStatus.Verdict = "Awaited"
 	expStatus.LastUpdateTime = metav1.Now()
 }
 
 // CompletedExperimentStatus fills up ExperimentStatus Structure with values chaosResult
-func (expStatus *ExperimentStatus) CompletedExperimentStatus(chaosResult *v1alpha1.ChaosResult, experimentDetails *ExperimentDetails) {
+func (expStatus *ExperimentStatus) CompletedExperimentStatus(chaosResult *v1alpha1.ChaosResult, engineName, experimentPodName string) {
 	//var currExpStatus v1alpha1.ExperimentStatuses
-	expStatus.Name = experimentDetails.JobName
-	expStatus.Status = "Execution Successful"
+	expStatus.Name = chaosResult.Spec.ExperimentName
+	expStatus.Runner = engineName + "-runner"
+	expStatus.ExpPod = experimentPodName
+	expStatus.Status = "Completed"
 	expStatus.LastUpdateTime = metav1.Now()
 	expStatus.Verdict = chaosResult.Status.ExperimentStatus.Verdict
-	//return currExpStatus
 }
 
 // NotFoundExperimentStatus initilize experiment struct using the following values.
-func (expStatus *ExperimentStatus) NotFoundExperimentStatus(experimentDetails *ExperimentDetails) {
-	expStatus.Name = experimentDetails.JobName
+func (expStatus *ExperimentStatus) NotFoundExperimentStatus(expName, engineName string) {
+	expStatus.Name = expName
+	expStatus.Runner = engineName + "-runner"
+	expStatus.ExpPod = "N/A"
 	expStatus.Status = "ChaosExperiment Not Found"
-	expStatus.Verdict = "Failed"
+	expStatus.Verdict = "Fail"
 	expStatus.LastUpdateTime = metav1.Now()
 }
