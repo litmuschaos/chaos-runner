30eaa90979af00168ca362a01eb7f7fd302405ed
ksatchit <karthik.s@mayadata.io>
2020-11-14 15:35:06 +0000

    (chore)vendor: update latest dependencies
    Signed-off-by: ksatchit <karthik.s@mayadata.io>

--- vendor/github.com/litmuschaos/chaos-operator/pkg/apis/litmuschaos/v1alpha1/chaosengine_types.go
+++ vendor/github.com/litmuschaos/chaos-operator/pkg/apis/litmuschaos/v1alpha1/chaosengine_types.go
@@ -141,6 +141,8 @@ type RunnerInfo struct {
 	ImagePullSecrets []corev1.LocalObjectReference `json:"imagePullSecrets,omitempty"`
 	// Runner Annotations that needs to be provided in the pod for pod that is getting created
 	RunnerAnnotation map[string]string `json:"runnerannotation,omitempty"`
+	// NodeSelector for runner pod
+	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
 	// ConfigMaps for runner pod
 	ConfigMaps []ConfigMap `json:"configMaps,omitempty"`
 	// Secrets for runner pod
@@ -223,11 +225,24 @@ type K8sCommand struct {
 type CmdProbeInputs struct {
 	// Command need to be executed for the probe
 	Command string `json:"command,omitempty"`
-	// Expected output or result of the command
-	ExpectedResult string `json:"expectedResult,omitempty"`
+	// Comparator check for the correctness of the probe output
+	Comparator ComparatorInfo `json:"comparator,omitempty"`
 	// The source where we have to run the command
 	// It can be a image or inline(inside experiment itself)
 	Source string `json:"source,omitempty"`
+}
+
+// ComparatorInfo contains the comparator details
+type ComparatorInfo struct {
+	// Type of data
+	// it can be int, float, string
+	Type string `json:"type,omitempty"`
+	// Criteria for matching data
+	// it supports >=, <=, ==, >, <, != for int and float
+	// it supports equal, notEqual, contains for string
+	Criteria string `json:"criteria,omitempty"`
+	// Value contains relative value for criteria
+	Value string `json:"value,omitempty"`
 }
 
 //HTTPProbeInputs contains all the inputs required for http probe
@@ -249,6 +264,8 @@ type RunProperty struct {
 	//ProbePollingInterval contains time interval, for which continuous probe should be sleep
 	// after each iteration
 	ProbePollingInterval int `json:"probePollingInterval,omitempty"`
+	//InitialDelaySeconds time interval for which probe will wait before run
+	InitialDelaySeconds int `json:"initialDelaySeconds,omitempty"`
 }
 
 // ExperimentComponents contains ENV, Configmaps and Secrets
