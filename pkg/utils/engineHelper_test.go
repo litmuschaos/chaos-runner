79acff858a3b0b8afeaef088b8a027edbfeeac90
Udit Gaurav <35391335+uditgaurav@users.noreply.github.com>
2021-03-15 15:13:02 +0530

    chore(test): Add unit test to chaos runner (#135)
    * chore(test): Add unit test to chaos runner
    
    Signed-off-by: udit <udit@chaosnative.com>

--- pkg/utils/engineHelper_test.go
+++ pkg/utils/engineHelper_test.go
@@ -111,12 +111,7 @@ func TestSetExpAnnotationFromEngine(t *testing.T) {
 
 	for name, mock := range tests {
 		t.Run(name, func(t *testing.T) {
-			client := CreateFakeClient(t)
 
-			_, err := client.LitmusClient.LitmuschaosV1alpha1().ChaosEngines(mock.chaosengine.Namespace).Create(mock.chaosengine)
-			if err != nil {
-				t.Fatalf("engine not created for %v test, err: %v", err, name)
-			}
 			expDetails := experiment.SetExpAnnotationFromEngine(mock.chaosengine)
 
 			actualResult := expDetails.Annotations
@@ -177,12 +172,7 @@ func TestSetResourceRequirementsFromEngine(t *testing.T) {
 
 	for name, mock := range tests {
 		t.Run(name, func(t *testing.T) {
-			client := CreateFakeClient(t)
 
-			_, err := client.LitmusClient.LitmuschaosV1alpha1().ChaosEngines(mock.chaosengine.Namespace).Create(mock.chaosengine)
-			if err != nil {
-				t.Fatalf("engine not created for %v test, err: %v", err, name)
-			}
 			expDetails := experiment.SetResourceRequirementsFromEngine(mock.chaosengine)
 			actualResult := expDetails.ResourceRequirements
 			expectedResult := mock.chaosengine.Spec.Experiments[0].Spec.Components.Resources
@@ -236,12 +226,7 @@ func TestSetImagePullSecretsFromEngine(t *testing.T) {
 
 	for name, mock := range tests {
 		t.Run(name, func(t *testing.T) {
-			client := CreateFakeClient(t)
 
-			_, err := client.LitmusClient.LitmuschaosV1alpha1().ChaosEngines(mock.chaosengine.Namespace).Create(mock.chaosengine)
-			if err != nil {
-				t.Fatalf("engine not created for %v test, err: %v", err, name)
-			}
 			expDetails := experiment.SetImagePullSecretsFromEngine(mock.chaosengine)
 			actualResult := expDetails.ImagePullSecrets[0].Name
 			expectedResult := fakeImagePullSecret
@@ -293,11 +278,6 @@ func TestSetExpNodeSelectorFromEngine(t *testing.T) {
 
 	for name, mock := range tests {
 		t.Run(name, func(t *testing.T) {
-			client := CreateFakeClient(t)
-			_, err := client.LitmusClient.LitmuschaosV1alpha1().ChaosEngines(mock.chaosengine.Namespace).Create(mock.chaosengine)
-			if err != nil {
-				t.Fatalf("engine not created for %v test, err: %v", err, name)
-			}
 			expDetails := experiment.SetExpNodeSelectorFromEngine(mock.chaosengine)
 			actualResult := expDetails.NodeSelector
 			expectedResult := fakeNodeSelector
@@ -353,17 +333,92 @@ func TestSetTolerationsFromEngine(t *testing.T) {
 
 	for name, mock := range tests {
 		t.Run(name, func(t *testing.T) {
-			client := CreateFakeClient(t)
 
-			_, err := client.LitmusClient.LitmuschaosV1alpha1().ChaosEngines(mock.chaosengine.Namespace).Create(mock.chaosengine)
-			if err != nil {
-				t.Fatalf("engine not created for %v test, err: %v", err, name)
-			}
 			expDetails := experiment.SetTolerationsFromEngine(mock.chaosengine)
 			actualResult := expDetails.Tolerations
 			expectedResult := mock.chaosengine.Spec.Experiments[0].Spec.Components.Tolerations
 			if !reflect.DeepEqual(expectedResult, actualResult) {
 				t.Fatalf("Test %q failed: expectedOutput is: '%v' but the actualOutput is: '%v'", name, expectedResult, actualResult)
+			}
+		})
+	}
+}
+
+func TestInstanceAttributeValuesFromChaosEngine(t *testing.T) {
+	fakeNewExperimentAnnotation := map[string]string{"my-fake-key": "myfake-val"}
+	fakeNodeSelector := make(map[string]string)
+	fakeImagePullSecret := "Fake Image Pull Secret"
+	fakeNewExperimentImage := "fake-new-experiment-image"
+	experiment := ExperimentDetails{
+		Name:               "Fake-Exp-Name",
+		Namespace:          "Fake NameSpace",
+		JobName:            "fake-job-name",
+		StatusCheckTimeout: 10,
+	}
+	engineDetails := EngineDetails{
+		Name:            "Fake Engine",
+		EngineNamespace: "Fake NameSpace",
+	}
+
+	tests := map[string]struct {
+		chaosengine *litmuschaosv1alpha1.ChaosEngine
+	}{
+		"Test Positive-1": {
+			chaosengine: &litmuschaosv1alpha1.ChaosEngine{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      engineDetails.Name,
+					Namespace: engineDetails.EngineNamespace,
+				},
+				Spec: litmuschaosv1alpha1.ChaosEngineSpec{
+					Experiments: []litmuschaosv1alpha1.ExperimentList{
+						{
+							Name: experiment.Name,
+							Spec: v1alpha1.ExperimentAttributes{
+								Components: v1alpha1.ExperimentComponents{
+									NodeSelector:          fakeNodeSelector,
+									ExperimentAnnotations: fakeNewExperimentAnnotation,
+									ExperimentImage:       fakeNewExperimentImage,
+									Resources: v1.ResourceRequirements{
+										Limits: v1.ResourceList{
+											"cpu":    resource.MustParse("500"),
+											"memory": resource.MustParse("100"),
+										},
+										Requests: v1.ResourceList{
+											"cpu":    resource.MustParse("10"),
+											"memory": resource.MustParse("5"),
+										},
+									},
+									Tolerations: []v1.Toleration{
+										{
+											Key:      "fake-key",
+											Operator: v1.TolerationOperator("Exists"),
+											Effect:   v1.TaintEffect("NoSchedule"),
+										},
+									},
+									ExperimentImagePullSecrets: []v1.LocalObjectReference{
+										{
+											Name: fakeImagePullSecret,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for name, mock := range tests {
+		t.Run(name, func(t *testing.T) {
+			client := CreateFakeClient(t)
+
+			_, err := client.LitmusClient.LitmuschaosV1alpha1().ChaosEngines(mock.chaosengine.Namespace).Create(mock.chaosengine)
+			if err != nil {
+				t.Fatalf("engine not created, err: %v", err)
+			}
+			if err = experiment.SetInstanceAttributeValuesFromChaosEngine(&engineDetails, client); err != nil {
+				t.Fatalf("%v test failed, err: %v", name, err)
 			}
 		})
 	}
