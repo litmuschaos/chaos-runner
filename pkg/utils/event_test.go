eee0ad0d0f4f511d7061b521e846fb11f7cd3390
uditgaurav <udit@chaosnative.com>
2022-10-06 13:49:27 +0530

    Chore(healthCheck): Update default healthCheck tunable in engine
    Signed-off-by: uditgaurav <udit@chaosnative.com>

--- pkg/utils/event_test.go
+++ pkg/utils/event_test.go
@@ -2,10 +2,11 @@ package utils
 
 import (
 	"context"
-	"github.com/stretchr/testify/require"
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
