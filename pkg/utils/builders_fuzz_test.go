a8eaaa69127ecde765e53cbc5570435e97df67b4
Sayan Mondal <sayan.mondal@harness.io>
2024-03-14 12:05:40 +0530

    test: Adding fuzz test for getSetSidecarSecrets in builders.go (#214)

--- pkg/utils/builders_fuzz_test.go
+++ pkg/utils/builders_fuzz_test.go
@@ -22,3 +22,29 @@ func FuzzGetEnvFromMap(f *testing.F) {
 		require.Equal(t, envCount, len(targetStruct.m)+1)
 	})
 }
+
+func FuzzSetSidecarSecrets(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		fuzzConsumer := fuzz.NewConsumer(data)
+		targetStruct := &struct {
+			experiment *ExperimentDetails
+		}{}
+
+		err := fuzzConsumer.GenerateStruct(targetStruct)
+		if err != nil {
+			return
+		}
+		if targetStruct.experiment != nil {
+			secrets := setSidecarSecrets(targetStruct.experiment)
+			require.GreaterOrEqual(t, len(secrets), 1)
+
+			for _, sidecar := range targetStruct.experiment.SideCars {
+				for _, secret := range sidecar.Secrets {
+					for _, s := range secrets {
+						require.Equal(t, s.Name, secret.Name)
+					}
+				}
+			}
+		}
+	})
+}
